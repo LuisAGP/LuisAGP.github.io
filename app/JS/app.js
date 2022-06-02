e4a99429b1ace560004982ae2ca8c4741a33cb34
Luis <luis.lagp4598@gmail.com>
2022-06-02 00:31:45 -0500

    Se desarrollo función para incluir tamplates dinamicamente

--- app/JS/app.js
+++ app/JS/app.js
@@ -1,24 +1,44 @@
-
+const urlBase = window.location.origin;
 
 window.onload = () => {
 
-
+    includeHtml();
 
 }
 
 
-// const includeHtml = () => {
+const includeHtml = () => {
 
-//     try {
-        
-//         fetch('../')
-//         .then(response => response.text())
-//         .then(text => {
-//             console.log(text);
-//         });
+    try {
 
-//     } catch (error) {
-//         console.error(error);
-//     }
+        let includers = document.querySelectorAll("div[include-html-file]")
 
-// }
+        for(let i of includers){
+
+            let url = i.getAttribute("include-html-file");
+            
+            fetch(urlBase+url)
+            .then(response => response.text())
+            .then(html => {
+
+                let parser = new DOMParser();
+                codigo = parser.parseFromString(html, 'text/html').body;
+
+                i.replaceWith(...codigo.childNodes);
+
+            })
+            .catch(error => console.log(error));
+
+        }
+
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+
+
+
+const dd = (message) => {
+    console.log(message);
+}
