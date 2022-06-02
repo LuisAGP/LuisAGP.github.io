593daea6ca8520c9099a8c2391f4a47a479afc0b
Luis <luis.lagp4598@gmail.com>
2022-06-01 23:17:15 -0500

    test

---
+++ app/JS/app.js
@@ -0,0 +1,24 @@
+
+
+window.onload = () => {
+
+
+
+}
+
+
+// const includeHtml = () => {
+
+//     try {
+        
+//         fetch('../')
+//         .then(response => response.text())
+//         .then(text => {
+//             console.log(text);
+//         });
+
+//     } catch (error) {
+//         console.error(error);
+//     }
+
+// }
